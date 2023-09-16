10e618b30bb99afb2232e0399d94f45e4e821c83
Seif Sekalala <dibleauzah@gmail.com>
2023-09-15 23:42:33 -0400

    sept 15 practice

---
+++ importantAlgos/main.js
@@ -0,0 +1,27 @@
+//!_Date: Sept 15
+
+//?_"Min, Max, and Avg."
+//?_Given an array, print the max, min and average values for that array.
+//*_1)--Open-Book--It's Ok to Look Before Testing the Code.
+//*_2)--I__Testing suspended (for today.) See farther below for additional related comment.
+
+let myArr = [1, 2, 3, 4, 5];
+var min = myArr[0];
+var max = myArr[0];
+var avg = 0;
+
+for (let i = 0; i < myArr.length; i++){
+    if (myArr[i] < min) min = myArr[i];
+    if (myArr[i] > max) max = myArr[i];
+    avg += myArr[i];
+}
+avg /= myArr.length;
+
+console.log("Min is : " + min);
+console.log("Max is : " + max);
+console.log("Average is " + avg);
+
+//*_2)--II__I am not sure--in fact, I really doubt (with my assigned odds against =  67%), that this code-block is going to run successfully. Regardless, I'm sticking to my plan as declared above, re: not testing it. Goal of not testint the code: to help ease mysele back into the habit of regular codin, versus coding "well".
+
+//!_End of Day
+//!-------------------------------//
