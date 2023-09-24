99614a053be80b43c007f59fe12877ddb9434c0a
Seif Sekalala <dibleauzah@gmail.com>
2023-09-23 23:40:34 -0400

    brief updates to data-sci directory

--- publicProjectsAndSnippets/app-set3-dataSci/mainPractice.py
+++ publicProjectsAndSnippets/app-set3-dataSci/mainPractice.py
@@ -1,3 +1,9 @@
 # number_list = [1, 2, 3]
 # avg = sum(number_list)/len(number_list)
 # print("The average is ", round(avg,2))
+
+import pandas as pd
+
+abalone = pd.read_csv('abalone.data', header = None)
+
+abalone.head(3)
