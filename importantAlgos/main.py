d8e43edfbdda2d71f2c6dede5428860f30e2eea1
Seif Sekalala <dibleauzah@gmail.com>
2023-10-04 23:21:28 -0400

    updating various sub-directories--Oct 4, 2023.

--- importantAlgos/main.py
+++ importantAlgos/main.py
@@ -8,21 +8,38 @@ ___Testing suspended.
 
 # def fizzBuzz(n):
 #     result = []
-    
 #     for i in range(1, n + 1):  
 #         add = ''
 #         if i % 3 == 0:
 #             add += 'Fizz'
-        
 #         if i % 5 == 0:
 #             add += 'Buzz'
-        
 #         if add == '':
 #             result.append(i)
 #         else:
 #             result.append(add)
-    
 #     return result 
 
 #!_//---------------------------------------------------// 
 
+'''
+//!_Oct 4
+___Same goal as previous entry above; i.e., keep up the momemtum with the final learning/practice.
+___"Is N a Prime Number?"
+'''
+# print("Testing-testing, 1-2-3...");
+
+# import math
+# def isNPrime (n):
+#     #?_Declaration of fact that all numbers less than 2 are not primes:
+#     if n < 2:
+#         return False
+    
+#     #?_Loop from 2 to sqrt(n):
+#     for i in range(2, int(math.ceil(math.sqrt(n)))):
+#         #?_Check if (n modulo i) is equal to 0, if so then there are two numbers--a and b, that can multiply to give n.    
+#         if n % i == 0:
+#             return False
+#         return True 
+
+#!_//---------------------------------------------------// 
