b5610ce3d2ffec430f5815f9d2c890b52fc974bc
Seif Sekalala <dibleauzah@gmail.com>
2023-10-09 22:31:36 -0400

    adding new java code-snippet

---
+++ importantAlgos/main.java
@@ -0,0 +1,14 @@
+// public class MyClass {
+
+//   private int x;
+
+//   public MyClass(int x) {
+//     this.x = x;
+//   }
+
+//   public void myMethod() {
+//     System.out.println(x);
+//   }
+// }
+
+//!_Testing suspended. 
