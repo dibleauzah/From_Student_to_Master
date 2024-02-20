23971c2780d19fcf89f281ef8ff7d5aa5f136692
Seif Sekalala <dibleauzah@gmail.com>
2024-02-20 10:17:31 -0500

    Day 1 of STM self-challenge.

---
+++ Feb-Apr_Self-Coach_Bt-Camp/1--Review and Practice/algos.js
@@ -0,0 +1,15 @@
+//!_Day 1 of Student-to-Master (STM) Self-Challenge: Feb 20, 2024
+//*_Self-encouragement note: Don't focus on sites or tools that discourage you. For instance: **IGNORE Hackerrank!**
+
+//!_Challenge #-1 (of STM Day 1); Coding: SumArray
+// function sumArray(arr){
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+// //?_Testing function:
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(sumArray(numbers));
+//?_Test result: successful.
---
+++ Feb-Apr_Self-Coach_Bt-Camp/1--Review and Practice/algos.py
@@ -0,0 +1,30 @@
+# _#!_Day 1 of Student-to-Master (STM) Self-Challenge: Feb 20, 2024
+# _#*_Self-encouragement note: Don't focus on sites or tools that discourage you. For instance: **IGNORE Hackerrank!**
+
+# _#!_Challenge #-2 (of STM Day 1); Coding: Reverse String
+
+def reverse_string(string):
+    def reverse_string(string):
+        return string[::-1]
+# ?_Testing function:
+input_string = "Hello world! :-)"
+print(reverse_string(input_string))
+# ?_Test result: Unsuccessful.
+
+#_#!=======================================================//
+# _#!_Challenge #-3 (of STM Day 1); Data Science: Student Average Scores
+
+# def calculate_average(scores):
+#     average_scores = {}
+#     for student, scores_list in scores.items():
+#         average_scores[student] = sum(scores_list) / len(scores_list)
+#     return average_scores
+
+# ?_Testing function:
+# student_scores = {
+#     'Alice': [80, 75, 90],
+#     'Bob': [85, 90, 95],
+#     'Charlie': [70, 75, 80]
+# }
+# print(calculate_average(student_scores))
+# ?_Test result: Successful.
