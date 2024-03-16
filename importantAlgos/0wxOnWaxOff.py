6e8053d6067faedfdad8a23572378b14923d7a36
Seif Sekalala <dibleauzah@gmail.com>
2024-03-16 02:59:41 -0400

    Adding Day 5 of STM self-challenge work(s).

--- importantAlgos/0wxOnWaxOff.py
+++ importantAlgos/0wxOnWaxOff.py
@@ -1,5 +1,5 @@
-#_#!_Day 2 of Student-to-Master (STM) Self-Challenge: Mar 13, 2024
-#_//#!_Executed in algos.js file.
+# _#!_Day 2 of Student-to-Master (STM) Self-Challenge: Mar 13, 2024
+# _//#!_Executed in algos.js file.
 
 #!-----------------------------------------------//
 
@@ -12,7 +12,7 @@
 #     for i in arr:
 #         applied = fn(i)
 #         result.append(applied)
-    
+
 #     print(result)
 
 # square = lambda x: x * x
@@ -20,5 +20,27 @@
 
 # map([1, 2, 3, 4], square) #_#!_ <== This should result in an output of "[1, 4, 9, 16]"
 # map([1, 2, 3, 4], addZeroes) #_#!_ <== This should result in an output of "[100, 200, 300, 400]"
+
+#!-----------------------------------------------//
+
+#!_Day 5 of STM Self-Challenge: Mar 16 (Sat), 2024
+#!_Challenge #-10__: "...Words With 3 Cont.- Vowels"
+
+import re
+# ?_Makes sure to look this up later: is the "import **_re_** a feature/library of an older version of Python, or is...". "NM," actually.
+
+# def threeVowels (string):
+#     #?_Split string into an array of words:
+#     arr = string.split('')
+#     count = 0
+
+#     #?_Search for this regex pattern:
+#     for word in arr:
+#         if re.search(r'[aeiou]{3, }', word) != None:
+#             count = += 1
+
+# return count
+# ?_Testing suspended for later.
+# ?_Rough time for completion: 5 to 10 mins. Good job, self/Se*_i_*f! ;-)
 
 #!-----------------------------------------------//
