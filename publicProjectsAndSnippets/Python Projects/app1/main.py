1aceea6ea3d304c31fdcce05faf54e8c12cc4bc7
Seif Sekalala <dibleauzah@gmail.com>
2024-04-15 15:30:38 -0400

    Adding general "Student to Master" progress for today, April 15.

---
+++ publicProjectsAndSnippets/Python Projects/app1/main.py
@@ -0,0 +1,29 @@
+# import random
+
+# # Generate a random number between 1 and 100
+# number = random.randint(1, 100)
+
+# # Initialize the guess to an invalid value
+# guess = -1
+
+# # Keep looping until the user guesses the correct number
+# while guess != number:
+#   # Prompt the user to guess a number
+#   guess_str = input("Enter your guess: ")
+
+#   # Convert the user's guess to an integer
+#   guess = int(guess_str)
+
+#   # Check if the user's guess is too low or too high
+#   if guess < number:
+#     print("Too low, try again. ")
+#   elif guess > number:
+#     print("Too high, try again. ")
+
+# # The user guessed the correct number!
+# print(f"Congratulations! You guessed the number {number} correctly! ")
+
+#?_Test result: Failure.
+#?_Other note(s):
+#_____#*_Date of attempt: April 15, 2024. 
+#_____#*_"Moving-Forward" Resolution: I'll just have to try it again later.
